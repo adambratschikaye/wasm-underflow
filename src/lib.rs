9f0feb2239bce59287a4c21051f7a713b80b6837
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2024-05-29 16:12:36 +0200

    cleaned up

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,3 @@
-use candid::{CandidType, Decode, Deserialize, Encode};
-use serde::Serialize;
-
-#[derive(CandidType, Deserialize, Serialize)]
-enum Tree {
-    Leaf,
-    SubTree(Vec<Tree>),
-}
-
-fn make_tree() -> Tree {
-    let n = 4096;
-
-    let mut tree = Tree::Leaf;
-    for _ in 0..n {
-        tree = Tree::SubTree(vec![tree]);
-    }
-
-    tree
-}
-
-#[no_mangle]
-pub fn candid_tree() {
-    let tree = make_tree();
-
-    let bytes = Encode!(&tree).unwrap();
-    Decode!(&bytes, Tree).unwrap();
-}
-
-#[no_mangle]
-pub fn json_tree() {
-    let tree = make_tree();
-
-    let bytes = serde_json::to_vec(&tree).unwrap();
-    let mut deserializer = serde_json::Deserializer::from_slice(&bytes);
-    deserializer.disable_recursion_limit();
-    let _result: Tree = serde::de::Deserialize::deserialize(&mut deserializer).unwrap();
-}
-
 const SIZE: usize = 16;
 
 struct Bar {
@@ -95,15 +57,10 @@ pub fn foo() {
     let mut init = Bar::new();
     let count = (1024 * 1024) / (SIZE as u64) + 2;
     bar(count, &mut init);
-    println!("done");
+    println!("checking vecs");
     for v in vecs {
         for b in v {
             assert_eq!(b, 0);
         }
     }
-}
-
-#[test]
-fn test_json() {
-    json_tree()
 }
